f39deefaf54fdf494ddf08f140e4565158885a16
hongjuns <asws12345@naver.com>
2023-08-03 22:49:58 +0900

    login / logout 처리

--- src/main/java/io/security/coresecurity/config/SecurityConfig.java
+++ src/main/java/io/security/coresecurity/config/SecurityConfig.java
@@ -51,8 +51,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
-
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .permitAll();
     }
 }
